bbfaee1f428973e53a453c4a6ef6184fd95d4d94
doczi-dominik <doczi.dominik0@gmail.com>
2022-02-22 10:23:59 +0100

    📝 Added BorderColorMediator documentation

--- Sources/game/mediators/BorderColorMediator.hx
+++ Sources/game/mediators/BorderColorMediator.hx
@@ -3,6 +3,15 @@ package game.mediators;
 import kha.Color;
 import game.boardstates.StandardBoardState;
 
+/**
+ * `BorderColorMediator` provides access to a StandardBoardState's border
+ * color changing ability. Notably, it's used in `AllClearManager` to change
+ * the border color to golden whenever an All Clear is triggered.
+ * 
+ * While a `StandardBoardState` could change its border color when triggering
+ * an All Clear, using the `BorderColorMediator` allows easy changing from e.g.
+ * the pause menu.
+ */
 class BorderColorMediator {
 	public var boardState(null, default): StandardBoardState;
 
