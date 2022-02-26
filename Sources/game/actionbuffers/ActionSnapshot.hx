d3230007992784d9cc4a1fea4869e3820cd26b09
doczi-dominik <doczi.dominik0@gmail.com>
2022-02-26 13:36:01 +0100

    🐛 Fixed toBitField calcualtion

--- Sources/game/actionbuffers/ActionSnapshot.hx
+++ Sources/game/actionbuffers/ActionSnapshot.hx
@@ -23,8 +23,8 @@ class ActionSnapshot {
 	public final hardDrop: Bool;
 
 	public function toBitField() {
-		return (shiftLeft ? 1 : 0) << 5
-			+ (shiftRight ? 1 : 0) << 4 + (rotateLeft ? 1 : 0) << 3 + (rotateRight ? 1 : 0) << 2 + (softDrop ? 1 : 0) << 1 + (hardDrop ? 1 : 0);
+		return (shiftLeft ? 1 << 5 : 0) + (shiftRight ? 1 << 4 : 0) + (rotateLeft ? 1 << 3 : 0) + (rotateRight ? 1 << 2 : 0) + (softDrop ? 1 << 1 : 0)
+			+ (hardDrop ? 1 : 0);
 	}
 
 	public function isNotEqual(other: ActionSnapshot) {
