64ee723108cb291d216c1577e5a223cd2e4cd08b
doczi-dominik <doczi.dominik0@gmail.com>
2022-02-27 14:42:42 +0100

    🧱 Added IReplayableGameMode

--- Sources/game/gamemodes/EndlessGameMode.hx
+++ Sources/game/gamemodes/EndlessGameMode.hx
@@ -5,7 +5,7 @@ import game.rules.Rule;
 import save_data.Profile;
 
 @:structInit
-class EndlessGameMode implements IGameMode {
+class EndlessGameMode implements IReplayableGameMode {
 	public final profile: Profile;
 	public final rngSeed: Int;
 	public final rule: Rule;
@@ -13,4 +13,13 @@ class EndlessGameMode implements IGameMode {
 	@:optional public final replayData: Null<ReplayData>;
 
 	public var gameMode(default, never) = GameMode.ENDLESS;
+
+	public function copyWithReplay(data: ReplayData) {
+		return ({
+			profile: profile,
+			rngSeed: rngSeed,
+			rule: rule,
+			replayData: data
+		} : EndlessGameMode);
+	}
 }
