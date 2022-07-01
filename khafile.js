ec110c649a8e7b2bc390dcd33ed625582d894d58
doczi-dominik <doczi.dominik0@gmail.com>
2022-07-01 21:21:58 +0200

    🔥 Removed Libraries folder

--- khafile.js
+++ khafile.js
@@ -3,7 +3,6 @@ let project = new Project('Project GelaVolt');
 project.addAssets('Assets/**');
 project.addShaders('Shaders/**');
 project.addSources('Sources');
-project.addLibrary('haxe-ws');
 
 project.windowOptions.width = 1920;
 project.windowOptions.height = 1080;
